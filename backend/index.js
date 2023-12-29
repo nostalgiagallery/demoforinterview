2e306c92ac3ffd7ace90f230584d36a0e54614e2
nostalgiagallery <nostalgiagallery.ng@gmail.com>
2023-12-29 21:27:40 +0530

    "git first push backend"

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const server = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dataRouters = require("./routes/data");
+
+// server.use(express.static(path.resolve(__dirname, "build")));
+server.use(cors());
+server.use(express.json());
+
+
+server.use("/data", dataRouters.router);
+
+
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URL);
+  console.log("mongoose connected");
+}
+
+server.listen(process.env.PORT, () => {
+  console.log("server started");
+});
